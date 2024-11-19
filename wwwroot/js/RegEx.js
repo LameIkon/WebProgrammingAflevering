a2dbb3809821f2e8507bf5cd53aab851dbb87ad6
LameIkon <troelsjelsbak@hotmail.com>
2024-11-19 11:18:27 +0100

    The same

--- wwwroot/js/RegEx.js
+++ wwwroot/js/RegEx.js
@@ -3,23 +3,29 @@ const USERNAME = document.getElementById('username');
 const PASSWORD = document.getElementById('password');
 const REPEAT_PASSWORD = document.getElementById('repeat-password');
 
+const ERROR_MESSAGE = 'error-message';
 
+const REGEX_NAME = /([A - Z])\w+/;
 
 function addErrorMessage(element)
 {
-    const ERROR_MESSAGE = 'error-message';
-
     return (`${element.id}-${ERROR_MESSAGE}`);
 };
-
-
-EMAIL.addEventListener('change', function (event)
-{
-    const MESSAGE = addErrorMessage(EMAIL);
-
-    const ERROR = document.getElementById(MESSAGE);
-    ERROR.innerText = EMAIL.value;
-});
     
 
-function newMessage() { }
+function addErrorMessageListener(element)
+{
+    if (element != null) {
+        element.addEventListener('change', function () {
+
+            const ERROR = document.getElementById(addErrorMessage(element));
+            ERROR.innerText = element.value;
+        })
+    }
+}
+
+
+addErrorMessageListener(EMAIL);
+addErrorMessageListener(USERNAME) ;
+addErrorMessageListener(PASSWORD);
+addErrorMessageListener(REPEAT_PASSWORD);

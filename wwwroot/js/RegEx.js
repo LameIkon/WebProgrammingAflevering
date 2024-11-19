b22cd3dedd9cd2f192710fd2abe2dcd07f7d923d
LameIkon <troelsjelsbak@hotmail.com>
2024-11-19 07:57:59 +0100

    On the verge of a break down!
    A ~ was needed yesterday, but broke the code today.

--- wwwroot/js/RegEx.js
+++ wwwroot/js/RegEx.js
@@ -1,16 +1,19 @@
-﻿
-const EMAIL = document.getElementById('email');
+﻿const EMAIL = document.getElementById('email');
 const USERNAME = document.getElementById('username');
 const PASSWORD = document.getElementById('password');
 const REPEAT_PASSWORD = document.getElementById('repeat-password');
 
 
 
-function ValidateInput()
+function addErrorMessage(element)
 {
-    
-    
-}
+    const ERROR_MESSAGE = 'error-message';
+
+    const ERROR_ID = (`${element.id}-${ERROR_MESSAGE}`);
+
+    console.log(ERROR_ID);
+};
+
 
 
 
